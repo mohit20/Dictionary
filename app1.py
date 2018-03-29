62e601f8b0b2b57e1e711002bad78b13f7cca33d
mohit20 <agarwal.mohit421@gmail.com>
2018-03-29 21:17:46 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,29 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def translate(word):
+	if word.lower() in data:
+		return data[word.lower()]
+	elif len(get_close_matches(word,data.keys())) > 0:
+		yn = input("Did you mean %s instead? Enter Y if yes, or N if no" % get_close_matches(word,data.keys())[0])	
+		if yn == "Y":
+			return data[get_close_matches(word,data.keys())[0]]	
+		elif yn == "N":
+			return "Word doesn't exists. Please try again."
+		else:
+			return "Please try again"
+	else:
+		return "The word doesn't exist. Please double check it" 
+
+
+word = input("Enter the word ")
+
+output = translate(word)
+
+if(type(output) == list):
+	for item in output:
+	print(item) 
+else:
+	print(output)
